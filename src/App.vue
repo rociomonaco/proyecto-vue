b06b7d18660289040b6530c66ba8f0d64453ac8a
romonaco <rociomonaco@gmail.com>
2022-03-12 19:43:22 -0300

    proyect food cli

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ItemProduct/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ItemProduct from './components/ItemProduct.vue'
+
+
+export default {
+  name: 'App',  
+  data (){
+    return {
+      products: [
+      {
+        nombre: "Spicy seasoned seafood noodles",
+        precio: 250,
+        imagen: "./assets/products/product-1.png",
+        categoria: "",
+        stock: 20 
+      },
+      {
+        nombre: "Spicy seasoned seafood noodles",
+        precio: 250,
+        imagen: "./assets/products/product-1.png",
+        categoria: "",
+        stock: 20 
+      },
+      {
+        nombre: "Spicy seasoned seafood noodles",
+        precio: 250,
+        imagen: "./assets/products/product-1.png",
+        categoria: "",
+        stock: 20 
+      }
+    ]
+    }
+  },
+  
+  components: {
+    HelloWorld,
+    ItemProduct
+  },
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ItemProduct.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- item -->
+  <div
+    class="w-2/5 p-5 item bg-slate-900 rounded-lg text-stone-50 mx-2 cursor-pointer">
+    <!-- img -->
+    <div class="relative w-full">
+      <img
+        class="absolute -top-20 left-8"
+        src=""
+        alt="product #1"
+      />
+    </div>
+    <!--/ img -->
+
+    <!-- text details -->
+    <div class="mt-20 text-center">
+      <h3 class="px-8 mb-2">Spicy seasoned seafood noodles</h3>
+      <p class="my-2">$ 2.29</p>
+      <p class="text-neutral-300 my-2">20 Bowls available</p>
+    </div>
+    <!--/ text details -->
+  </div>
+  <!--/ item -->
+</template>
+
+<script>
+export default {
+  name: "ItemProduct",
+  props: {
+    nombre: String,
+    precio: Number,
+    categoria: String,
+    imagen: String,
+    stock: Number,
+    seleccionado: Boolean
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
